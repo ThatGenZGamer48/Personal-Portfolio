4b68a17111808cf8af8b0b62cc36a3aeac2403e3
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-23 15:00:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+export default function App() {
+	return (
+		<div className="container">
+			<Navbar />
+			<Content />
+			<Footer />
+		</div>
+	);
+}
