483d9f2b6aedb42a9f522ff0c898849770f8a46a
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-23 15:04:22 +0530

    Create Content.js

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Content() {
+    return (
+        <div className="content">
+            <h1>The content goes here...</h1>
+        </div>
+    );
+}
