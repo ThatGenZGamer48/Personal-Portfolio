1d91fb89976a1e67dca2a1dea6f27304d8289a44
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-23 15:05:32 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h1>The footer goes here...</h1>
+        </div>
+    )
+};
